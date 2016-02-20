30b9fca94f8336b2f8890a64380b168c05c20bc7
陳泰澄 <john987john987@gmail.com>
2016-02-20 16:28:19 +0800

    修正橡皮擦
    x,y顛倒

--- main.py
+++ main.py
@@ -27,31 +27,25 @@ def main():
 @app.route('/')
 def index():
     return render_template('index.htm')
-@app.route('/b/<room_id>')
-def showTestBrain(room_id):
-    print("使用" + str(room_id) + "房間", file = sys.stderr)
-    return render_template('brain.htm')
+@app.route('/b/<room_id>', methods = ["POST", "GET"])
+def showBrain(room_id):
+    if request.method == "POST":
+        user_id = request.form['user_id']
+        print(str(room_id) + "房間，" + str(user_id) + "上傳圖片", file = sys.stderr)
+        DataURL = request.form['DataURL'] # datauri = 'data:image/png;base64,iVBORw0K...'
+        output = open('room/' + room_id + '_' + user_id + '.png', 'wb')
+        output.write(base64.b64decode(re.search(r'base64,(.*)', DataURL).group(1).encode()))
+        output.close()
+        return "ok"
+    else:
+        print("使用" + str(room_id) + "房間", file = sys.stderr)
+        return render_template('brain.htm')
 @app.route('/b/test_view')
 def showReadOnlyTestBrain():
     return render_template('brainReadOnly.htm')
 @app.route('/upload', methods = ["POST", "GET"])
 def upload():
-    if request.method == "POST":
-        user_id = request.form['user_id']
-        room_id = request.form['room_id']
-        print(str(room_id) + "房間，" + str(user_id) + "上傳圖片", file = sys.stderr)
-        DataURL = request.form['DataURL']
-        # datauri = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
-        # imgstr = re.search(r'base64,(.*)', datauri).group(1)
-        # print(imgstr, "\n\n", file = sys.stderr)
-
-        imgstr = re.search(r'base64,(.*)', DataURL).group(1)
-        output = open('room/' + room_id + '_' + user_id + '.png', 'wb')
-        output.write(base64.b64decode(imgstr.encode()))
-        output.close()
-        return "ok"
-    else:
-        return render_template('upload.htm')
+    return render_template('upload.htm')
 # @app.route('/b/<brainName>')
 # def showBrain(brainName):
 # 	return render_template('brain.htm')
