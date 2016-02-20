2b32cf199b41cc6a886ed71fa83e2e917ab05856
陳泰澄 <john987john987@gmail.com>
2016-02-20 17:49:04 +0800

    房間號碼修改，擦子修正
    加入bootstrap color pinker
    使用localStorage
    擦子修正
    Server簡化

--- main.py
+++ main.py
@@ -7,21 +7,10 @@ import re
 # ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__, static_url_path = "", static_folder = "static")
 app.debug = True
-app.config['UPLOAD_FOLDER'] = os.getcwd() + "/room/"
-# def allowed_file(filename):
-#     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-# @app.route('/upup', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         file = request.files['file']
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#             return redirect(url_for('uploaded_file', filename = filename))
-#     return ''
-@app.route('/room/<path:filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+# app.config['UPLOAD_FOLDER'] = os.getcwd() + "/room/"
+# @app.route('/room/<path:filename>') # png檔
+# def uploaded_file(filename):
+#     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 def main():
     app.run(host = "0.0.0.0", port = 80)
 @app.route('/')
@@ -33,21 +22,18 @@ def showBrain(room_id):
         user_id = request.form['user_id']
         print(str(room_id) + "房間，" + str(user_id) + "上傳圖片", file = sys.stderr)
         DataURL = request.form['DataURL'] # datauri = 'data:image/png;base64,iVBORw0K...'
-        output = open('room/' + room_id + '_' + user_id + '.png', 'wb')
-        output.write(base64.b64decode(re.search(r'base64,(.*)', DataURL).group(1).encode()))
-        output.close()
-        return "ok"
+        ImageData = base64.b64decode(re.search(r'base64,(.*)', DataURL).group(1).encode())
+        with open('static/room/' + room_id + '_' + user_id + '.png', 'wb') as output:
+            output.write(ImageData)
+        return "<img src=" + url_for('static', filename= 'room/' + room_id + '_' + user_id + '.png') + " />"
     else:
         print("使用" + str(room_id) + "房間", file = sys.stderr)
         return render_template('brain.htm')
-@app.route('/b/test_view')
-def showReadOnlyTestBrain():
+@app.route('/b_status/<room_id>') # readonly
+def showBrainStatus(room_id):
+    return "good"
+@app.route('/b2/<room_id>') # readonly
+def showReadOnlyTestBrain(room_id):
     return render_template('brainReadOnly.htm')
-@app.route('/upload', methods = ["POST", "GET"])
-def upload():
-    return render_template('upload.htm')
-# @app.route('/b/<brainName>')
-# def showBrain(brainName):
-# 	return render_template('brain.htm')
 if __name__ == '__main__':
     main()
